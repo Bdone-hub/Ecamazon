c14985d05ade75269febe812289abf5ac86aee59
Bdone-hub <67092457+Bdone-hub@users.noreply.github.com>
2023-11-30 12:44:30 +0100

    fonctionnel mais avec l'app en local et le dockeur db

---
+++ DB/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE mytable (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    age INT
+);
--- backend/app.py
+++ backend/app.py
@@ -1,36 +1,27 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-# Configuration de la base de données SQLite (remplacez 'sqlite:///test.db' par le chemin de votre base de données)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Configuration de la base de données
+db = mysql.connector.connect(
+    host=os.environ.get("DB_HOST", "127.0.0.1"),
+    user=os.environ.get("DB_USER", "myuser"),
+    password=os.environ.get("DB_PASSWORD", "mypassword"),
+    database=os.environ.get("DB_DATABASE", "mydatabase"),
+    port=int(os.environ.get("DB_PORT", 3306))
+)
 
-db = SQLAlchemy(app)
-
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
+cursor = db.cursor()
 
 @app.route('/')
-def hello_world():
-    # Ajouter un message à la base de données pour la démonstration
-    with app.app_context():
-        new_message = Message(content='This is a message from the database!')
-        db.session.add(new_message)
-        db.session.commit()
-
-        # Récupérer tous les messages de la base de données
-        messages = Message.query.all()
-        message_content = [message.content for message in messages]
-
-    return jsonify(message_content)
+def index():
+    cursor.execute("SELECT * FROM mytable")
+    result = cursor.fetchall()
+    return jsonify(result)
 
 if __name__ == '__main__':
-    # Créer la table dans la base de données
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, host='0.0.0.0')
